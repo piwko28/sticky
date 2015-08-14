74a2259946df5b594fdda68b6d0f6ca3eedeac81
Szymon Piwowarczyk <szymon@piwowarczyk.net>
2015-08-14 13:17:47 +0200

    Updating position can be disabled. Adding sticky class when the element reach the top edge.

--- js/sticky.js
+++ js/sticky.js
@@ -12,6 +12,7 @@
 		var defaults = {
 			scrollContainer : window,
 			parentContainer : $("body"),
+			updatePosition : true
 		};
 		var settings = $.extend({}, defaults, options);
 		var stickyContainer = this;
@@ -21,16 +22,25 @@
 			var stickyContainerTop = stickyContainer[0].offsetTop - parseInt(stickyContainer.css('margin-top'));
 			$(settings.scrollContainer).on('scroll', function() {
 			  var scroll = $(settings.scrollContainer).scrollTop();
+			  if(settings.updatePosition) {
 			  	stickyContainerTop = stickyContainer[0].offsetTop - parseInt(stickyContainer.css('margin-top'));
+			  }
+			  if(scroll > stickyContainerTop) {
+			  	stickyContainer.addClass("sticky");
+			  } else {
+			  	stickyContainer.removeClass("sticky");
+			  }
 			  stickyTop = Math.max(0,
 			  	Math.min(
 			  		scroll - stickyContainerTop,
 			  		$(settings.parentContainer).outerHeight() - stickyContainer.outerHeight(true)
 			  	)
 			  );
+			  if(settings.updatePosition) {
 				  stickyContainer.css({
 				  	transform : 'translateY(' + stickyTop + 'px)'
 				  });
+				}
 			});
 		}
 
